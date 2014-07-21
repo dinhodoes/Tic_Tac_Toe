acb0d1b5ca2deca048db806c3cc738bab777ebcb
dinhodoes <jahardy34@gmail.com>
2014-07-20 19:56:33 -0700

    added a little CSS to each box and added clickability

--- xoxo.js
+++ xoxo.js
@@ -1,24 +1,29 @@
-var player = 1
-
-function clickBox(box) {
-	var currentBox = document.getElementById(box);
-
-	if (currentBox.innerHTML == "") {
-		if(player == 1) {
-			currentBox.innerHTML = "X";
-			player -= 1;
-		}
-		else  {
-			currentBox.innerHTML = "O";
-			player += 1;}
-};
-};
-	
-// module
 var xoxoApp = angular.module('xoxoApp', []); 
-// defines controller
+
 xoxoApp.controller('GameController', function ($scope) { 
    
-    $scope.boxes = []
+    $scope.boxes = ['','','','','','','','',''];
+	var currentBox = 'X';
+	$scope.player = 1;
 
-   });
+	$scope.clickBtn = function(btn) {
+		console.log(btn);
+		if ($scope.boxes[btn] == '') {
+	 		if ($scope.player == 1) {
+	 			$scope.boxes[btn] = 'X';
+	 			$scope.player -= 1;
+	 		} else {
+	 			$scope.boxes[btn] = 'O';
+	 			$scope.player += 1;
+	 		};
+	 	};
+	  };
+});
+
+
+
+
+    
+
+
+ 

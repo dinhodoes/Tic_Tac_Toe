e3163a926147dd4a4cc4c309a4ac69c86837db8f
dinhodoes <jahardy34@gmail.com>
2014-07-22 15:27:04 -0700

    still working through the win logic…

--- xoxo.js
+++ xoxo.js
@@ -1,13 +1,14 @@
 var xoxoApp = angular.module('xoxoApp', []); 
-
 xoxoApp.controller('GameController', function ($scope) { 
    
     $scope.boxes = ['','','','','','','','',''];
-	var currentBox = 'X';
 	$scope.player = 1;
 
+	
+	$scope.check = function() {
+	 			};
+
 	$scope.clickBtn = function(btn) {
-		console.log(btn);
 		if ($scope.boxes[btn] == '') {
 	 		if ($scope.player == 1) {
 	 			$scope.boxes[btn] = 'X';
@@ -16,10 +17,42 @@ xoxoApp.controller('GameController', function ($scope) {
 	 			$scope.boxes[btn] = 'O';
 	 			$scope.player += 1;
 	 		};
+	 		
 	 	};
-	  };
-});
+	 	
 
+	var combos = [
+		[0,1,2],
+		[3,4,5],
+		[6,7,8],
+		[0,3,6],
+		[1,4,7],
+		[2,5,8],
+		[0,4,8],
+		[2,4,6]
+		];
+
+
+
+	 	for (var i = 0; i < combos.length; i++) {
+	 		var xcount = 0;
+	 		var ocount = 0;
+	 			for (var j = 0; j < combos[i].length; j++) {
+	 				if (combos[i][j] == 'X') {
+	 					xcount += 1;
+	 						if (xcount == 3)
+	 						alert('X wins!');
+	 				} else if (combos[i][j] == 'O') {
+	 					ocount += 1;
+	 						if (ocount == 3)
+	 						alert('O wins!');
+	 				}
+	 			}
+			}		
+
+	 };
+	
+});
 
 
 

dca68e71fad9080ef8bb7540b8be8f51b21d84db
dinhodoes <jahardy34@gmail.com>
2014-07-23 17:37:57 -0700

    prefirebase trial. new light grey boxes

--- JS/xoxo.js
+++ JS/xoxo.js
@@ -2,25 +2,24 @@
 
 var xoxoApp = angular.module('xoxoApp', ['firebase']); 
 xoxoApp.controller('GameController', function ($scope, $firebase) { 
-  
+
+	$scope.clickBtn = $firebase(new Firebase("https://xoxo.firebaseio.com/clickBtn"));
+
+	$scope.remoteboxes = $firebase(new Firebase("https://xoxo.firebaseio.com/remoteboxes"));
+
+	
+
 	var myDataRef = new Firebase('https://xoxo.firebaseio.com/'); 
 
     $scope.boxes = ['','','','','','','','',''];
 	$scope.player = 1;
 
-	
 	var combos = [
-		[0,1,2],
-		[3,4,5],
-		[6,7,8],
-		[0,3,6],
-		[1,4,7],
-		[2,5,8],
-		[0,4,8],
-		[2,4,6]
-		];
+		[0,1,2],[3,4,5],[6,7,8],[0,3,6],
+		[1,4,7],[2,5,8],[0,4,8],[2,4,6]
+	];
 
-
+	// Checking to see if there's a winner
 	$scope.check = function() {
 	 	for (var i = 0; i < combos.length; i++) {
 	 		var xcount = 0;
@@ -29,18 +28,22 @@ xoxoApp.controller('GameController', function ($scope, $firebase) {
 	 			if ($scope.boxes[combos[i][j]] == 'X') {
 	 				xcount += 1;
 	 					if (xcount == 3)
-	 						alert('X wins!');
+	 					alert('X wins!');
 	 			} else if ($scope.boxes[combos[i][j]] == 'O') {
 	 				ocount += 1;
 	 					if (ocount == 3)
-	 						alert('O wins!');
+	 					alert('O wins!');
 	 			}
 	 		}
 		}		
 
 	};
-
-	
+	// Reset Function
+	$scope.reset = function() {
+		$scope.boxes = ['','','','','','','','',''];
+		$scope.player = 1;
+	};
+	// Clicking buttons 
 	$scope.clickBtn = function(btn) {
 		if ($scope.boxes[btn] == '') {
 	 		if ($scope.player == 1) {
@@ -50,12 +53,12 @@ xoxoApp.controller('GameController', function ($scope, $firebase) {
 	 			$scope.boxes[btn] = 'O';
 	 			$scope.player += 1;
 	 		};
-	 		$scope.check(combos);
+	 		$scope.check();
+	 		// $scope.reset();	
+
 	 	};	
 	};
 
-	
-	
 	
 });
 
